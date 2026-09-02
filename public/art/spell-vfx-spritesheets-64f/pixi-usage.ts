6f6bd66597c7464dfeab94d9ffce04a983ada981
Mark Pearce <mark.pearce72@gmail.com>
2026-09-02 13:38:21 +0200

    vfx lab

--- public/art/spell-vfx-spritesheets-64f/pixi-usage.ts
+++
@@ -1,8 +0,0 @@
-import { AnimatedSprite, Assets } from 'pixi.js';
-
-const sheet = await Assets.load('blast.json');
-const effect = new AnimatedSprite(sheet.animations.blast);
-effect.anchor.set(0.5);
-effect.animationSpeed = 0.2; // 12 fps on a 60 fps Pixi ticker
-effect.loop = false;
-effect.play();
